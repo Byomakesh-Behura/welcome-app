89e6bd956e2c3a570345b1f096e53ee5eb6221a4
Byomakesh <byomakeshbehura@gmail.com>
2025-01-25 12:01:15 +0530

    adding one more endpoint

--- Welcome_App/src/main/java/com/example/demo/controller/WelcomeController.java
+++ Welcome_App/src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class WelcomeController {
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to my world";
+	}
+	
+	@GetMapping("/wish/{name}")
+	public String wish(@PathVariable String name) {
+		return "Good Morning "+name;
 	}
 
 }
